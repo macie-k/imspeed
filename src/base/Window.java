f793d13ea1351f4cf8134b4a836f62cefedc97c0
emermacko <mr.macko.yt@gmail.com>
2020-03-15 19:01:46 +0100

    Add files via upload

--- src/base/Window.java
+++ src/base/Window.java
@@ -1,52 +1,24 @@
 package base;
 
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import menu.Selection;
 
-import menu.*;
 
 public class Window extends Application {
 		
-	private static int x=0;
-	
 	private Pane root = new Pane();
 	
-	private Languages[] lngs = new Languages[5];
-		
 	@Override
 	public void start(Stage stage) throws Exception {	
 		root.setPrefSize(800, 600);
 		Scene scene = new Scene(root, Color.web("#0f0f0f"));
 		
-		for(int i=0; i<5; i++) {
-			int calcY = 200 + 20*i;
-			lngs[i] = new Languages(calcY, Words.loadWords(x)[i], x==i);
-		}
-		root.getChildren().addAll(lngs);
-		stage.setScene(scene);
-		
-		scene.setOnKeyPressed(e -> {
-			switch (e.getCode()) {
-				case UP: if(x>0) x--;
-					root.getChildren().removeAll(lngs);
-					break;
-				case DOWN: if(x<4) x++;
-					root.getChildren().removeAll(lngs);
-					break;
-				default: break;
-				
-			}
-			for(int i=0; i<5; i++) {
-				int calcY = 200 + 20*i;
-				lngs[i] = new Languages(calcY, Words.loadWords(x)[i], x==i);
-			}
-			
-			root.getChildren().addAll(lngs);
-			stage.setScene(scene);
-		});
+		Selection.select(scene, root, stage);	// run menu scene
 		
 		stage.setTitle("I'm speed");
 		stage.setResizable(false);
