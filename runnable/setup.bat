a7ce50ee95e9505b5c8ee7946d5046c8740abef6
emermacko <mr.macko.yt@gmail.com>
2020-03-22 12:51:57 +0100

    Environment variables setup for .exe

--- runnable/setup.bat
+++ runnable/setup.bat
@@ -8,20 +8,20 @@ echo 	$err = $true >> setup.ps1
 echo } >> setup.ps1
 echo Write-Host "" >> setup.ps1
 echo if(!$err){ >> setup.ps1
-echo 	Write-Host "Is:" -nonewline; Write-Host " $java " -f yellow -nonewline; $path = Read-Host -Prompt "path to SDK? [y/n]" >> setup.ps1
+echo 	Write-Host "Is:" -nonewline; Write-Host " $java " -f yellow -nonewline; $path = Read-Host -Prompt "path to JDK? [y/n]" >> setup.ps1
 echo 	if($path -eq "y"){ >> setup.ps1
 echo 		$path = $java; >> setup.ps1
 echo 	} else { >> setup.ps1
-echo 		$path = Read-Host -Prompt 'Path to SDK root directory' >> setup.ps1
+echo 		$path = Read-Host -Prompt 'Path to JDK root directory' >> setup.ps1
 echo 	} >> setup.ps1
 echo } else { >> setup.ps1
-echo 	$path = Read-Host -Prompt 'Path to SDK root directory' >> setup.ps1
+echo 	$path = Read-Host -Prompt 'Path to JDK root directory' >> setup.ps1
 echo } >> setup.ps1
 echo Write-Host "" >> setup.ps1
-echo [System.Environment]::SetEnvironmentVariable("JAVA_JDK", $path, [System.EnvironmentVariableTarget]::User) 
+echo [System.Environment]::SetEnvironmentVariable("JAVA_JDK", $path, [System.EnvironmentVariableTarget]::User) >> setup.ps1
 echo if($?){ >> setup.ps1
 echo 	Write-Host "JAVA_JDK" -f yellow -nonewline; Write-Host " Was successfully set to:  " -nonewline; Write-Host "$path" -ForegroundColor green >> setup.ps1
-echo 	Remove-Item $MyINvocation.InvocationName >> setup.ps1
+echo 	Remove-Item -LiteralPath $MyInvocation.MyCommand.Path -Force >> setup.ps1
 echo } else { >> setup.ps1
 echo 	Write-Host "Error, check if you have permissions" -ForegroundColor red >> setup.ps1
 echo } >> setup.ps1
