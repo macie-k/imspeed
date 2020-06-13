65731e97821d11269d3330f1593799cba8e45ea6
emermacko <mr.macko.yt@gmail.com>
2020-06-13 21:39:12 +0200

    More clear output

--- src/run.bat
+++ src/run.bat
@@ -3,6 +3,10 @@
 javac -cp ".\base\*;.\menu\*;..\words\*;" .\base\Window.java
 java -cp ".\base\*;.\menu\*;..\words\*;" base.Window 2> error_log.txt
 
+echo
+echo Deleting .class files . . .
+echo ----------------------------
 del /s /q *.class
+echo ----------------------------
 
 pause
