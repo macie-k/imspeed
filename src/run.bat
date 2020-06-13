da2ee46a7ad494bee95b8035c33cfe7c83f17188
emermacko <mr.macko.yt@gmail.com>
2020-06-14 01:59:16 +0200

    Update for new program version

--- src/run.bat
+++ src/run.bat
@@ -1,12 +1,23 @@
 @echo off
 
-javac -cp ".\base\*;.\menu\*;..\words\*;" .\base\Window.java
-java -cp ".\base\*;.\menu\*;..\words\*;" base.Window 2> error_log.txt
+ECHO.
+ECHO Temporarly moving 'jrt-fs.jar' file to AppData. . .
+MOVE /Y "%JAVA_HOME%\lib\jrt-fs.jar" "%APPDATA%" > nul
 
-echo
-echo Deleting .class files . . .
-echo ----------------------------
-del /s /q *.class
-echo ----------------------------
+SET CLASSPATH=".\base\*;.\menu\*;..\words\*;"
+SET JFX="%JAVA_HOME%\lib"
+SET MODULES="javafx.controls,javafx.base,javafx.graphics"
+
+javac --module-path %JFX% --add-modules %MODULES% -cp %CLASSPATH% .\base\Window.java
+java --module-path %JFX% --add-modules %MODULES% -cp %CLASSPATH% base.Window
+
+ECHO Restoring 'jrt-fs.jar' file . . .
+ECHO.
+MOVE /Y "%APPDATA%\jrt-fs.jar" "%JAVA_HOME%\lib" > nul
+
+ECHO Removing class files . . .
+ECHO.
+ECHO.
+del /s /q *.class > nul
 
 pause
