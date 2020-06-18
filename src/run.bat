4f1970cf3dd95216841768380e5599648a5785f5
emermacko <mr.macko.yt@gmail.com>
2020-06-19 01:17:03 +0200

    Update run.bat

--- src/run.bat
+++ src/run.bat
@@ -1,19 +1,11 @@
 @echo off
 
 ECHO.
-ECHO Temporarly moving 'jrt-fs.jar' file to AppData. . .
-MOVE /Y "%JAVA_HOME%\lib\jrt-fs.jar" "%APPDATA%" > nul
 
 SET CLASSPATH=".\base\*;.\menu\*;..\words\*;"
-SET JFX="%JAVA_HOME%\lib"
-SET MODULES="javafx.controls,javafx.base,javafx.graphics"
 
-javac --module-path %JFX% --add-modules %MODULES% -cp %CLASSPATH% .\base\Window.java
-java --module-path %JFX% --add-modules %MODULES% -cp %CLASSPATH% base.Window
-
-ECHO Restoring 'jrt-fs.jar' file . . .
-ECHO.
-MOVE /Y "%APPDATA%\jrt-fs.jar" "%JAVA_HOME%\lib" > nul
+javac -cp %CLASSPATH% .\base\Window.java
+java -cp %CLASSPATH% base.Window
 
 ECHO Removing class files . . .
 ECHO.
