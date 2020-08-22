327044f0a5fea30b637b5fe0d77d00826ff44508
emermacko <mr.macko.yt@gmail.com>
2020-08-22 22:31:53 +0200

    Added 'checked' property
    - added some setters and getters
    - renamed variables

--- src/menu/obj/Option.java
+++ src/menu/obj/Option.java
@@ -10,46 +10,61 @@ import javafx.scene.text.Text;
 
 public class Option extends StackPane {
 	
-	private final Text val;
-	private final Text check;
+	private final Text value;
 	private final Rectangle background;
+	private boolean checked = false;
+	private Text checkVal;
 	
+	/* Constructor for difficulties */
 	public Option(int y, String value, boolean highlighted) {
 		this(y, "", value, highlighted);
 	}
 	
-	public Option(int y, String checked, String value, boolean highlighted) {	
+	public Option(int y, String checkVal, String value, boolean highlighted) {	
 						
 		int textX = 240;
 		int bgX = 240;
 		int bgWidth = 320;
 		
-		check = new Text(checked);
-		check.setTranslateX(250);
-		check.setTranslateY(y);
-		check.setFont(Font.font("Courier new", 14));
+		this.checkVal = new Text(checkVal);
+		this.checkVal.setTranslateX(250);
+		this.checkVal.setTranslateY(y);
+		this.checkVal.setFont(Font.font("Courier new", 14));
 		
-		val = new Text(value);
-		val.setTranslateX(textX);
-		val.setTranslateY(y);
-		val.setFont(Font.font("Courier new", 14));
+		this.value = new Text(value);
+		this.value.setTranslateX(textX);
+		this.value.setTranslateY(y);
+		this.value.setFont(Font.font("Courier new", 14));
 		
 		background = new Rectangle(bgWidth, 25, Color.WHITE);
 		background.setTranslateX(bgX);
 		background.setTranslateY(y);
 		
-		if(highlighted) {
-			background.setFill(Color.WHITE);
-			val.setFill(Color.BLACK);
-			check.setFill(Color.BLACK);
-		} else {
-			background.setFill(Window.BACKGROUND);
-			val.setFill(Color.WHITE);
-			check.setFill(Color.WHITE);
-		}
+		this.setHighlighted(highlighted);
 		
-		setAlignment(val, Pos.CENTER);
-		setAlignment(check, Pos.CENTER_LEFT);
-		getChildren().addAll(background, val, check);	
+		setAlignment(this.value, Pos.CENTER);
+		setAlignment(this.checkVal, Pos.CENTER_LEFT);
+		getChildren().addAll(background, this.value, this.checkVal);	
+	}
+	
+	public void setChecked(boolean checked) {
+		this.checkVal.setText(checked ? "[ x ]" : "[   ]");
+		this.checked = checked;
+	}
+	
+	public boolean getChecked() {
+		return this.checked;
+	}
+	
+	public void setHighlighted(boolean highlighted) {
+		if(highlighted) {
+			this.background.setFill(Color.WHITE);
+			this.value.setFill(Color.BLACK);
+			this.checkVal.setFill(Color.BLACK);
+		} else {
+			this.background.setFill(Window.BACKGROUND);
+			this.value.setFill(Color.WHITE);
+			this.checkVal.setFill(Color.WHITE);
+		}
 	}
 }
