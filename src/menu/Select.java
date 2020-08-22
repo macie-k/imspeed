2ac990ebebfedc61a8ed874352ab938228acc199
emermacko <mr.macko.yt@gmail.com>
2020-08-22 22:35:11 +0200

    Significantly refactored menu selection mechanics
    - custom difficulty is no longer resetting
    - one-lined some ifs
    - fixed console feedback
    - changed variables to final

--- src/menu/Select.java
+++ src/menu/Select.java
@@ -17,17 +17,15 @@ import base.Window;
 
 public class Select {
 	
-	public static List<File> selected_lng_files= new ArrayList<File>();
-	public static List<String> selected_lng_names = new ArrayList<String>();
+	public static final List<File> selected_lng_files= new ArrayList<File>();
+	public static final List<String> selected_lng_names = new ArrayList<String>();
 	
-	public static boolean disable_move = false;
 	public static int[] x = {4, 4, 4};
 	
-	private static int row=0;
-	private static Stage window = Window.window;
+	private static int row;
 	private static int how_many_lngs;
 	
-	private static String[] loadedDifficulties;
+	private final static Stage window = Window.window;
 
 	
 	/* set difficulty */
@@ -36,45 +34,28 @@ public class Select {
 		row=0;
 		
 		Pane root = Scenes.selectMenu("DIFFICULTY");
-		Scene scene = new Scene(root);
-		
-		loadedDifficulties = Words.loadDifficulties();
-		
-		Option[] diff = new Option[5];		
-		for(int i=0; i<4; i++) {
-			int calcY = 220 + 25*i;
-			diff[i] = new Option(calcY, loadedDifficulties[i], row==i);
-		} diff[4] = new Option(345, loadedDifficulties[4], row==4);
-		
-		root.getChildren().addAll(diff);			
+		Scene scene = new Scene(root);		
+
 		window.setScene(scene);
 		
 		scene.setOnKeyPressed(e -> {
 			switch (e.getCode()) {
 			case UP:
-				if(row>0) {
-					row--;
-				} else {
-					row = 4;
-				}
-				setScene(root, scene, "diff", diff);
+				row = (row > 0) ? row-1 : 4;
+				setHighlight(Scenes.diffs);
 				break;
 				
 			case DOWN:
-				if(row<4) {
-					row++;
-				} else {
-					row = 0;
-				}
-				setScene(root, scene, "diff", diff);
+				row = (row < 4) ? row+1 : 0;
+				setHighlight(Scenes.diffs);
 				break;
 				
 			case ENTER:
-				Window.DIFFICULTY = row+1;
-				System.out.println("[OK] Difficulty: " + loadedDifficulties[row]);
-				if(row==4) {
-					customDifficulty();
+				Window.gameDifficulty = row+1;
+				if(row == 4) {
+					customDifficulty(false);
 				} else {
+					System.out.println("[OK] Difficulty: " + Scenes.loadedDifficulties[row]);
 					selectLanguage(false);
 				}
 				break;
@@ -84,20 +65,26 @@ public class Select {
 			
 		});
 	}
-
+	
 	/* custom difficulty selection */
-	public static void customDifficulty() {
+	public static void customDifficulty(boolean infinite) {
 		
-		row=0; x[0] = 4; x[1]=4; x[2]=4;
+		row=0; 
 		Scenes.pointer.setTranslateY(0);
 		
 		Pane root = Scenes.selectMenu("CUSTOM");
-		Scene scene = new Scene(root);							
+		Scene scene = new Scene(root);		
+		
+		for(int i=0; i<3; i++) {
+			for(int j=0; j<=x[i]; j++) {
+				Scenes.scales[i][j].setFilled(true);
+			}
+		}
 				
 		window.setScene(scene);
 		
 		scene.setOnKeyPressed(e -> {
-			
+						
 			double arrowY = Scenes.pointer.getTranslateY();
 			
 			switch (e.getCode()) {
@@ -136,9 +123,11 @@ public class Select {
 				break;
 				
 			case ENTER:
-				Window.DIFFICULTY = 5;
+				Window.gameDifficulty = 5;
+				
 				int hF = x[0]+1; int hO = x[1]+1; int hM = x[2]+1;
-				System.out.println("[OK] Difficulty: Custom [" + hF + ":" + hO + ":" + hM + "]");
+				System.out.println("[OK] Difficulty: Custom [" + hF + ":" + hO + ":" + hM + "]" + ((infinite) ? " :: Infinite" : ""));
+				
 				parseCustom(hF, hO, hM);
 				selectLanguage(true);
 				break;
@@ -154,11 +143,6 @@ public class Select {
 		Window.howFast = 1_650_000_000 - 150_000_000*howFast;
 		Window.howOften = 8_000_000_000l - 500_000_000*howOften;
 		Window.multiplierAdd = 0.05 + ((howFast-5) + (howOften-5) + (howMany-5))/100;
-		
-//		alternative version 
-//		if(howFast >= 5) {
-//			Window.howFast = 1_125_000_000l - 75_000_000*howFast;
-//		}
 	}
 	
 	/* select languages */
@@ -169,9 +153,7 @@ public class Select {
 		selected_lng_names.clear();
 		row=0;
 					
-		String[][] loadedLanguages = Words.loadLanguages();
-		
-		if(loadedLanguages == null) {
+		if(Words.loadLanguages() == null) {
 			Window.error("MISSING_WORDS");
 			return;
 		}
@@ -179,37 +161,20 @@ public class Select {
 		Pane root = Scenes.selectMenu("LANGUAGES");		
 		Scene scene = new Scene(root);
 		
-		how_many_lngs = Words.how_many_lngs;	// redefine variable to avoid `0`
-		
-		/* load all available languages */
-		Option[] lngs = new Option[how_many_lngs];
-		for(int i=0; i<how_many_lngs; i++) {
-			int calcY = 220 + 25*i;
-			lngs[i] = new Option(calcY, loadedLanguages[i][0], loadedLanguages[i][1], row==i);
-		}
-		
-		root.getChildren().addAll(lngs);
 		window.setScene(scene);
+		how_many_lngs = Words.how_many_lngs;	// redefine variable to avoid `0`
 				
 		/* menu movement key listener */
 		scene.setOnKeyPressed(e -> {
 			switch (e.getCode()) {		
 			case UP:
-				if(row>0) {
-					row--;
-				} else {
-					row = how_many_lngs-1;
-				}
-				setScene(root, scene, "lng", lngs);
+				row = (row > 0) ? row-1 : how_many_lngs-1;
+				setHighlight(Scenes.lngs);
 				break;
 				
 			case DOWN:
-				if(row<how_many_lngs-1) {
-					row++;
-				} else {
-					row = 0;
-				}
-				setScene(root, scene, "lng", lngs);
+				row = (row < how_many_lngs-1) ? row+1 : 0;
+				setHighlight(Scenes.lngs);
 				break;
 				
 			case SPACE:
@@ -222,7 +187,7 @@ public class Select {
 						selected_lng_files.add(Words.listOfFiles[row]);
 					}
 				}
-				setScene(root, scene, "lng", lngs);
+				setCheck();
 				break;
 				
 			case ENTER:
@@ -232,55 +197,38 @@ public class Select {
 						selected_lng_files.add(Words.listOfFiles[row]);
 					}
 				}
-				
-				setScene(root, scene, "lng", lngs);
-				
+								
 				if(!Words.loadWords(selected_lng_files).isEmpty()) {
 						System.out.print("[OK] Languages: ");
-						selected_lng_names.forEach(sln -> System.out.print("{" + sln + "} ")); System.out.println("\n");
+						selected_lng_names.forEach(sln -> System.out.print("{" + sln + "} ")); System.out.println();
 					Window.startGame(selected_lng_files);
 				}
 				break;
 				
-			case ESCAPE: 
+			case ESCAPE:
 				if(custom) {
-					customDifficulty();
+					customDifficulty(Window.infinite);
 				} else {
 					selectDifficulty();
 				}
 				break;
 			default: break;			
 			}
+			
 		});
 	}
 	
-	/* method for refreshing the view // kind of uneccesary */
-	static void setScene(Pane root, Scene scene, String type, Option[] option) {
-		
-		root.getChildren().removeAll(option);
-		
-		switch(type) {
-		case "diff":
-			for(int i=0; i<4; i++) {
-				int calcY = 220 + 25*i;
-				option[i] = new Option(calcY, loadedDifficulties[i], i==row);
-			}
-			
-			option[4] = new Option(345, loadedDifficulties[4], 4==row);
-		break;
-		
-		case "lng":
-			for(int i=0; i<how_many_lngs; i++) {
-				int calcY = 220 + 25*i;
-				option[i] = new Option(calcY, Words.loadLanguages()[i][0], Words.loadLanguages()[i][1], i==row);
-			}
-		break;
-		}
-				
-		root.getChildren().addAll(option);
-		window.setScene(scene);
+	static void setCheck() {
+		Scenes.lngs[row].setChecked(!Scenes.lngs[row].getChecked());
 	}
 	
+	static void setHighlight(Option[] tab) {
+		int index = 0;
+		for (Option o : tab) {
+			o.setHighlighted(index++ == row);
+		}
+	}
+		
 	static boolean isEmpty(int y) {
 		try {
 			return !(Files.lines(Paths.get(Words.listOfFiles[y].toString())).count() > 1);
